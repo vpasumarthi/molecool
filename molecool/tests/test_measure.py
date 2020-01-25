f0620ff0664871343e7e0132c78409004c10b174
Viswanath Pasumarthi <pasumart@buffalo.edu>
2020-01-25 16:27:59 -0500

    pytest parametrize with many angles

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -41,3 +41,14 @@ def test_calculate_angle():
     expected_angle_in_degrees = 90
     calculated_angle_in_degrees = molecool.calculate_angle(r1, r2, r3, degrees=True)
     assert calculated_angle_in_degrees == expected_angle_in_degrees
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle_in_degrees", [
+(np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0, 0, 0]), np.array([0, 1, 0]), 45),
+(np.array([1, 0, 0]), np.array([0, 0, 0]), np.array([0, 1, 0]), 90)
+])
+def test_calculate_angle_many(p1, p2, p3, expected_angle_in_degrees):
+    """test will pass so long as `calculate_angle` function within `measure`
+    module run as expected"""
+
+    calculated_angle_in_degrees = molecool.calculate_angle(p1, p2, p3, degrees=True)
+    assert calculated_angle_in_degrees == expected_angle_in_degrees
