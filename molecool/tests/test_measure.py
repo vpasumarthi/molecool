6e5da2229b8ae5069e1b4957bab1a93b0ba7c1e8
Viswanath Pasumarthi <pasumart@buffalo.edu>
2020-01-25 15:46:25 -0500

    separate module-level test to a function-level test

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -9,17 +9,24 @@ import sys
 import numpy as np
 
 
-def test_measure():
-    """Sample test, will always pass so long as functions within 'measure'
+def test_calculate_distance():
+    """test will pass so long as `calculate_distance` function within `measure`
+    module run as expected"""
+
+    r1 = np.array([1, 0, 0])
+    r2 = np.array([0, 0, 0])
+
+    expected_distance = 1.0
+    calculated_distance = molecool.calculate_distance(r1, r2)
+    assert calculated_distance == expected_distance
+
+def test_calculate_angle():
+    """test will pass so long as `calculate_angle` function within `measure`
     module run as expected"""
 
     r1 = np.array([1, 0, 0])
     r2 = np.array([0, 0, 0])
     r3 = np.array([0, 1, 0])
-
-    expected_distance = 1.0
-    calculated_distance = molecool.calculate_distance(r1, r2)
-    assert calculated_distance == expected_distance
 
     expected_angle_in_degrees = 90
     calculated_angle_in_degrees = molecool.calculate_angle(r1, r2, r3, degrees=True)
