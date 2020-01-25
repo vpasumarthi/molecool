b4346911f422a646dbc520ec14dfbf871d3195fa
Viswanath Pasumarthi <pasumart@buffalo.edu>
2020-01-25 13:27:15 -0500

    move atom data to a new module file

--- molecool/functions.py
+++ molecool/functions.py
@@ -43,9 +43,8 @@ atomic_weights = {
     'Br': 79.904,
 }
 
-
 def open_xyz(file_location):
-    
+
     # Open an xyz file and return symbols and coordinates.
     xyz_file = np.genfromtxt(fname=file_location, skip_header=2, dtype='unicode')
     symbols = xyz_file[:,0]
@@ -54,49 +53,49 @@ def open_xyz(file_location):
     return symbols, coords
 
 def write_xyz(file_location, symbols, coordinates):
-    
+
     # Write an xyz file given a file location, symbols, and coordinates.
     num_atoms = len(symbols)
-    
+
     with open(file_location, 'w+') as f:
         f.write('{}\n'.format(num_atoms))
         f.write('XYZ file\n')
-        
+
         for i in range(num_atoms):
-            f.write('{}\t{}\t{}\t{}\n'.format(symbols[i], 
+            f.write('{}\t{}\t{}\t{}\n'.format(symbols[i],
                                               coordinates[i,0], coordinates[i,1], coordinates[i,2]))
 
 def draw_molecule(coordinates, symbols, draw_bonds=None, save_location=None, dpi=300):
-    
+
     # Draw a picture of a molecule using matplotlib.
-    
+
     # Create figure
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    
+
     # Get colors - based on atom name
     colors = []
     for atom in symbols:
         colors.append(atom_colors[atom])
-    
+
     size = np.array(plt.rcParams['lines.markersize'] ** 2)*200/(len(coordinates))
 
     ax.scatter(coordinates[:,0], coordinates[:,1], coordinates[:,2], marker="o",
                edgecolors='k', facecolors=colors, alpha=1, s=size)
-    
+
     # Draw bonds
     if draw_bonds:
         for atoms, bond_length in draw_bonds.items():
             atom1 = atoms[0]
             atom2 = atoms[1]
-            
+
             ax.plot(coordinates[[atom1,atom2], 0], coordinates[[atom1,atom2], 1],
                     coordinates[[atom1,atom2], 2], color='k')
-    
+
     # Save figure
     if save_location:
         plt.savefig(save_location, dpi=dpi, graph_min=0, graph_max=2)
-    
+
     return ax
 
 def calculate_angle(rA, rB, rC, degrees=False):
@@ -113,31 +112,31 @@ def calculate_angle(rA, rB, rC, degrees=False):
 
 def bond_histogram(bond_list, save_location=None, dpi=300, graph_min=0, graph_max=2):
     # Draw a histogram of bond lengths based on a bond_list (output from build_bond_list function)
-    
-    
+
+
     lengths = []
     for atoms, bond_length in bond_list.items():
         lengths.append(bond_length)
-    
+
     bins = np.linspace(graph_min, graph_max)
-    
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    
+
     plt.xlabel('Bond Length (angstrom)')
     plt.ylabel('Number of Bonds')
-    
-    
+
+
     ax.hist(lengths, bins=bins)
-    
+
     # Save figure
     if save_location:
         plt.savefig(save_location, dpi=dpi)
-    
+
     return ax
-        
+
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
-    
+
     # Find the bonds in a molecule (set of coordinates) based on distance criteria.
     bonds = {}
     num_atoms = len(coordinates)
@@ -149,18 +148,6 @@ def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
                 bonds[(atom1, atom2)] = distance
 
     return bonds
-
-atom_colors = {
-    'H': 'white',
-    'C': '#D3D3D3',
-    'N': '#add8e6',
-    'O': 'red',
-    'P': '#FFA500',
-    'F': '#FFFFE0',
-    'Cl': '#98FB98',
-    'Br': '#F4A460',
-    'S': 'yellow'
-}
 
 def canvas(with_attribution=True):
     """
