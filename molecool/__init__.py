be2fe4c98715e9f96e96effbb3261616f69b53c1
Viswanath Pasumarthi <pasumart@buffalo.edu>
2020-01-25 14:20:29 -0500

    setup import routines in __init__.py

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -5,6 +5,12 @@ A python package for analyzing and visualizing xyz file. For MolSSI Best Practic
 
 # Add imports here
 from .functions import *
+from .atom_data import atom_colors, atomic_weights
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import draw_bond_histogram, draw_molecule
+
+import molecool.io
 
 # Handle versioneer
 from ._version import get_versions
---
+++ molecool/io/__init__.py
@@ -0,0 +1,9 @@
+"""
+IO
+
+A python subpackage for reading and writing pdb, xyz files.
+"""
+
+# Add imports here
+from .pdb import open_pdb
+from .xyz import open_xyz, write_xyz
