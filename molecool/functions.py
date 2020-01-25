09eb81d4c39f579778528a0a21dd4bda350a01c8
Viswanath Pasumarthi <vpasumarthi@users.noreply.github.com>
2020-01-25 11:50:33 -0500

    Modify quote printed with running canvas

--- molecool/functions.py
+++ molecool/functions.py
@@ -23,7 +23,7 @@ def canvas(with_attribution=True):
         Compiled string including quote and optional attribution
     """
 
-    quote = "The code is but a canvas to our imagination!!!"
+    quote = "Python is not only cool, it is MOLEcool!!!"
     if with_attribution:
         quote += "\n\t- Adapted from Henry David Thoreau"
     return quote
